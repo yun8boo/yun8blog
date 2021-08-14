818d400fe77970e5ea576c6a22c7540e8ee3e4de
yun8boo <yuma051521@gmail.com>
2021-08-14 22:44:25 +0900

    シンタックスハイライト対応

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -5,10 +5,11 @@ interface Props {
 }
 
 const ListItem = ({content}: Props) => {
+  const ogpImage = `https://res.cloudinary.com/yun8boo/image/upload/l_text:Sawarabi%20Gothic_45:${content.title},co_rgb:333,w_500,c_fit/v1628947657/ogp_qysqpg.png`
   return (
     <div className={styles.container}>
       <div className={styles.image_wrapper}>
-        <img className={styles.thumbnail} src={content.thumbnail.url} />
+        <img className={styles.thumbnail} src={ogpImage} />
       </div>
       <div className={styles.wrapper}>
         <p>{content.title}</p>
