786990d7cbd493838ca690f1e478e6f2ab16d080
yun8boo <yuma051521@gmail.com>
2021-08-15 16:55:56 +0900

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,9 @@ function MyApp({ Component, pageProps }: AppProps) {
     <Layout>
       <Head>
         <meta name="theme-color" content="#f69435" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
         <link rel="manifest" href="/manifest.webmanifest" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
     </Layout>
