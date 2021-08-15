9fff469093fbc337f9378c3ff54bef0bd3fae0d4
yun8boo <yuma051521@gmail.com>
2021-08-15 16:43:58 +0900

    fix

--- src/components/domain/post/ListItem/ListItem.tsx
+++ src/components/domain/post/ListItem/ListItem.tsx
@@ -6,7 +6,7 @@ const ListItem = ({content}: Props) => {
   const ogpImage = `https://res.cloudinary.com/yun8boo/image/upload/l_text:Sawarabi%20Gothic_45:${content.title},co_rgb:333,w_500,c_fit/v1628947657/ogp_qysqpg.png`
   return (
     <div className='shadow-md rounded-md w-full'>
-      <div className='w-full sm:h-200px'>
+      <div className='w-full h-postImage'>
         <img className='object-cover rounded-tl-md rounded-tr-md w-full h-full ' src={ogpImage} />
       </div>
       <div className='p-5'>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    height: {
+      postImage: '200px'
+    }
   },
   variants: {
     extend: {},
