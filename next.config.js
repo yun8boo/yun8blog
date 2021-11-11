56fad5b5255ae96b9fbd5b526968a0904da3faf8
yuma.watanabe <yuma051521@gmail.com>
2021-11-11 15:25:02 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const path = require('path')
 
 module.exports = {
+  images: {
+    domains: ['images.microcms-assets.io'],
+  },
   webpack(config, options) {
     config.resolve.alias['@'] = path.join(__dirname, 'src')
     return config
